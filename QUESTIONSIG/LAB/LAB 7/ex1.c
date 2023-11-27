7b19e270e55589152ff730d8a1a68ad3eada8dfd
Kesav1316 <kesavram1316@gmail.com>
2023-11-27 17:58:49 +0530

    Added lab 7 files

--- QUESTIONSIG/LAB/LAB 7/ex1.c
+++ QUESTIONSIG/LAB/LAB 7/ex1.c
@@ -4,25 +4,29 @@
 
 *******************************************************************************/
 #include <stdio.h>
+#include <math.h>
 
-int
-sum_n_cubes (int n)
-{
+int sum_n_cubes (int n) {
 // Your CODE goes HERE
+  int sum = 0;
+  if (n==0) {
+    return 0;
+  }
 
+  sum+= (n*n*n) + sum_n_cubes(n-1);
+
+return sum;
 // Till HERE
 }
 
-int
-main ()
-{
+int main () {
   int num;
   printf ("Enter n - ");
   scanf ("%d", &num);
   int rec_result = sum_n_cubes (num);
   // Update the formula here
   int formula_result;
-
+  formula_result = pow((num*(num+1)/2),2);
   printf ("\nSum of cubes till %d is %d (recursion)", num, rec_result);
   printf ("\nSum of cubes till %d is %d (formula)", num, formula_result);
   return 0;
--- QUESTIONSIG/LAB/LAB 7/ex2.c
+++ QUESTIONSIG/LAB/LAB 7/ex2.c
@@ -1,37 +1,40 @@
 /******************************************************************************
 
-            Experiment - 2 Recursion
+Experiment - 2 Recursion
 
 *******************************************************************************/
 #include <stdio.h>
-
-float
-evaluate_polynomial (float *a, float x, int n)
-{
+#include <math.h>
+float evaluate_polynomial (float *a, float x, int n)  {
 // Your CODE goes HERE
-
+int out;
+if (n != 0) {
+  out = a[n-1] * pow(x, n-1) + evaluate_polynomial(a, x, n-1);
+} 
+else return 0;
+return out;
 // Till HERE
 }
 
 int
 main ()
 {
-  int x;
-  float *t;
-  // The below polynomial is 3x^3 + 4x^2  + 5 
-  float arr[3] = { 5, 4, 3 };
-  t = &arr[0];
+int x;
+float *t;
+// The below polynomial is 3x^2 + 4x + 5
+float arr[3] = { 5, 4, 3 };
+t = &arr[0];
 
-  // Notice here a way to find the size of the array 
-  int sz = sizeof (arr) / sizeof (float);
+// Notice here a way to find the size of the array
+int sz = sizeof (arr) / sizeof (float);
 
-  printf ("Enter value of x - ");
-  scanf ("%d", &x);
-  int rec_result = evaluate_polynomial (t, x, sz);
-  // Update the formula here
-  int formula_result;
+printf ("Enter value of x - ");
+scanf ("%d", &x);
+int rec_result = evaluate_polynomial (t, x, sz);
+// Update the formula here
+int formula_result;
 
-  printf ("\nSum of cubes till %d is %d (recursion)", x, rec_result);
-  printf ("\nSum of cubes till %d is %d (formula)", x, formula_result);
-  return 0;
+printf ("\nSum of cubes till %d is %d (recursion)", x, rec_result);
+printf ("\nSum of cubes till %d is %d (formula)", x, formula_result);
+return 0;
 }
